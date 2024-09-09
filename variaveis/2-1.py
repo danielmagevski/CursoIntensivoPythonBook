05052403e9fdaa170fbd742534a5d6bb9a2cb1ad
Daniel Magevski <daniel_magevski@hotmail.com>
2024-09-09 11:54:08 -0300

    exercicios-variaveis

---
+++ variaveis/2-1.py
@@ -0,0 +1,8 @@
+
+"""
+2.1 - Mensagem simples: Armazene uma mensagem em uma variável e, em seguida,
+exiba essa mensagem.
+"""
+msg = "Hello World"
+
+print(msg)
---
+++ variaveis/2-2.py
@@ -0,0 +1,11 @@
+"""
+2.2- Mensagens simples: Armazene uma mensagem em uma variável e, em seguida,
+exiba essa mensagem. 
+Então altere o valor de sua variável para uma nova mensagem e mostre essa nova
+mensagem.
+"""
+
+msg = "Hello World"
+print(msg)
+msg = "Hello Python"
+print(msg)
