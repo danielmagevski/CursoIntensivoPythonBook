f2dc6b145b5dd6de75f6322549a8083b913cc09a
Daniel Magevski <daniel_magevski@hotmail.com>
2024-09-17 23:25:36 -0300

    update strings

---
+++ strings/2-10.py
@@ -0,0 +1,8 @@
+"""
+2.9 – Número favorito: Use uma variavel para respresentar seu número favorito.
+Em seguida, usando essa variável, crie uma mensagem que revele seu número favorito
+e exiba a mensagem.
+"""
+n = 8
+msg = f'Meu número favorito é {n}.'
+print(msg)
---
+++ strings/2-9.py
@@ -0,0 +1,11 @@
+"""
+2.9 – Número oito: Escreva operações de adição, subtração, multiplicação e 
+divisão que resultem no número 8. Lembre-se de colocar suas operações em 
+instruções print para ver os resultados. Você deve criar quatro linhas como 
+esta: print(5 + 3) Sua saída deve simplesmente ser composta de quatro linhas,
+com o número 8 em cada uma das linhas.
+"""
+print(8-0)
+print(4*2)
+print(16/2)
+print(4+4)
